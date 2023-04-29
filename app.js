6cf14fa57874f6d1945d6b168bcfedeb16062d22
sanudev <sanujana7551@gmail.com>
2023-04-29 17:54:13 +0530

    add expence route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/connectdb.js';
 import router from './routes/userRoutes.js';
+import expence from './routes/AddExpence.js'
 import bodyparser from 'body-parser';
 
 const app=express();
@@ -20,7 +21,7 @@ connectDB(DATABASE_URL);
 app.use(express.json());
 
 app.use('/',router);
-
+app.use('/expence',expence);
 
 
 
