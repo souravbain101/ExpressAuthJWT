43d9e1a3435a294d5987e5913879a055127ce59b
Sourav Bain <souravbain8@gmail.com>
2023-05-01 12:01:36 +0530

    authentication server

--- controller/userController.js
+++ controller/userController.js
@@ -147,15 +147,17 @@ static UserPasswordReset=async(req,res)=>{
 }
 
 static refresh_token=async(req,res)=>{
+    console.log('api call');
     let token
     const {authorization}=req.headers
     if (authorization && authorization.startsWith('Bearer')) {
         try {
             //get token from header
             token=authorization.split(' ')[1];
-
+            console.log(token);
             //verify token
             const {userID}=jwt.verify(token,process.env.jwt_Secret);
+            
             if (userID) {
                 const access_token=jwt.sign({userID:userID},process.env.jwt_secret,{expiresIn:'1h'});
                 const refresh_token=jwt.sign({userID:userID},process.env.jwt_secret,{expiresIn:'1.5h'});
